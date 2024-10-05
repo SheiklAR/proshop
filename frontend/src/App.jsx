9908816adbc9e8d94116e9bce0cf9ea52afc8255
Sheik Abdul Rahman H <sheikabdulrahman2000@gmail.com>
2024-10-06 02:47:54 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar"
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      App</div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='font-bold'>NavBar</div>
+  )
+}
+
+export default NavBar
