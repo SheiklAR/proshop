981f61c5e039f3dc4e99ed3dbf5d439d4f5864b8
Sheik Abdul Rahman H <sheikabdulrahman2000@gmail.com>
2024-10-11 20:14:50 +0530

    created custom async error handler and made data come from db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
 import dotenv from 'dotenv';
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 connectDB();
+import router from "./routes/productRoutes.js";
 
 const app = express();
 
@@ -12,13 +12,6 @@ app.get('/api', (req, res) => {
     res.send("Api is running...")
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', router)
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT} port`));
