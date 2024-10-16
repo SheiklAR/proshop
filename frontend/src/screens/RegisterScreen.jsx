2ed4539429e1207b2fc6cd9f269a85f176690e3d
Sheik Abdul Rahman H <sheikabdulrahman2000@gmail.com>
2024-10-17 01:35:26 +0530

    Created Register Screen

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import LoginPage from "../components/LoginPage";
-import { useLoginMutation } from "../slices/usersSlice";
+import { useLoginMutation, useRegisterMutation } from "../slices/usersSlice";
 import { setCredentials } from "../slices/authSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useLocation, Link } from "react-router-dom";
@@ -8,14 +8,17 @@ import { toast } from "react-toastify";
 import Loader from "../components/Loader";
 
 
-const LoginScreen = () => {
+const RegisterScreen = () => {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
     
-    const [login, { isLoading }] = useLoginMutation();
+    const [register, { isLoading }] = useRegisterMutation();
+
     const { userInfo } = useSelector((state) => state.auth);
 
 
@@ -32,23 +35,38 @@ const LoginScreen = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        try {
-            const res = await login({ email, password }).unwrap();
-            dispatch(setCredentials({ ...res, }));
-            navigate(redirect);
-        } catch (err) {
-            toast.error(err?.data?.message || err.error);
+
+        if (confirmPassword != password) {
+            toast.error("Passwords do not match");
+            return
+        } else {
+            try {
+                const res = await register({name, email, password }).unwrap();
+                dispatch(setCredentials({ ...res, }));
+                navigate(redirect);
+            } catch (err) {
+                toast.error(err?.data?.message || err.error);
+            }
         }
+       
 
     }
 
     return  <>
         
     <div className='max-w-sm mx-auto py-3'>
-        <h1 className='text-3xl font-semibold text-gray-600 py-2'>Sign In</h1>
+        <h1 className='text-3xl font-semibold text-gray-600 py-2'>Sign Up</h1>
         <form onSubmit={handleSubmit}
             className='bg-slate-100 p-8 my-2 rounded-md'
         >
+            <div className="mb-5">
+                <label className="block mb-2 text-sm font-medium text-gray-500">Enter Your Name</label>
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="input-box" placeholder="Enter name" />
+            </div>
             <div className="mb-5">
                 <label className="block mb-2 text-sm font-medium text-gray-500">Your email</label>
                 <input
@@ -66,17 +84,26 @@ const LoginScreen = () => {
                     className="input-box"
                     placeholder='Enter Password'/>
             </div>
+            <div className="mb-5">
+                <label className="block mb-2 text-sm font-medium text-gray-500">Confirm Password</label>
+                <input
+                    type="password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    className="input-box"
+                    placeholder='Confirm Password'/>
+            </div>
         
-            <button type="submit" className="btn" disabled={isLoading}>Sign In</button>
+            <button type="submit" className="btn" disabled={isLoading}>Sign Up</button>
 
             {isLoading && <Loader />}
 
-                <p className='py-2 text-gray-500'>Don't have an account?
-                    <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}
-                        className='font-semibold hover:underline'>Sign Up</Link></p>
+                <p className='py-2 text-gray-500'>Already have an account?
+                    <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}
+                        className='font-semibold hover:underline'>Sign In</Link></p>
         </form>
     </div>
 </>
 }
 
-export default LoginScreen
+export default RegisterScreen
