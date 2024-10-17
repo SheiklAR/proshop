f216b871338882d389863c139696c6832c736c19
Sheik Abdul Rahman H <sheikabdulrahman2000@gmail.com>
2024-10-17 13:48:19 +0530

    created check out steps

---
+++ frontend/src/components/CheckoutSteps.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const CheckoutSteps = ({step1, step2, step3, step4}) => {
+    return <>
+        <div className='max-w-sm mx-auto my-2 p-2 flex justify-between items-center'>
+            <div className='text-base font-semibold text-gray-700'>
+                {step1 ? <Link to="/login">Sign In</Link>
+                    : <button disabled style={{ color: 'gray' }}>Sign In</button>}
+            </div>
+            <div className='text-base font-semibold text-gray-700'>
+                {step2 ? <Link to="/shipping">Shipping</Link>
+                    : <button disabled style={{ color: 'gray' }}>Shipping</button>}
+            </div>
+            <div className='text-base font-semibold text-gray-700'>
+                {step3 ? <Link to="/payment">payment</Link>
+                    : <button disabled style={{ color: 'gray' }}>Payment</button>}
+            </div>
+            <div className='text-base font-semibold text-gray-700'>
+                {step4 ? <Link to="/">Order</Link>
+                    : <button disabled style={{ color: 'gray' }}>Order</button>}
+            </div>
+        </div>
+    </>
+}
+
+export default CheckoutSteps
--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { saveShippingAddress } from "../slices/cartSlice";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingScreen = () => {
     const { shippingAddress } = useSelector((state) => state.cart);
@@ -20,50 +21,50 @@ const ShippingScreen = () => {
         navigate('/payment')
     }
 
-  return <>
-        
-  <div className='max-w-sm mx-auto py-3'>
-      <h1 className='text-3xl font-semibold text-gray-600 py-2'>Shipping Address</h1>
-      <form onSubmit={handleSubmit}
-          className='bg-slate-100 p-8 my-2 rounded-md'
-      >
-          <div className="mb-5">
-              <label className="block mb-2 text-sm font-medium text-gray-500">Address</label>
-              <input
-                  type="text"
-                  value={address}
-                  onChange={(e) => setAddress(e.target.value)}
-                  className="input-box" placeholder="Enter Address" />
-          </div>
-          <div className="mb-5">
-              <label className="block mb-2 text-sm font-medium text-gray-500">City</label>
-              <input
-                  type="text"
-                  value={city}
-                  onChange={(e) => setCity(e.target.value)}
-                  className="input-box" placeholder="Enter City" />
-          </div>
-          <div className="mb-5">
-              <label className="block mb-2 text-sm font-medium text-gray-500">Pin Code</label>
-              <input
-                  type="text"
-                  value={pinCode}
-                  onChange={(e) => setPinCode(e.target.value)}
-                  className="input-box" placeholder="Enter Pincode (Postalcode)" />
-          </div>
-          <div className="mb-5">
-              <label className="block mb-2 text-sm font-medium text-gray-500">Country</label>
-              <input
-                  type="text"
-                  value={country}
-                  onChange={(e) => setCountry(e.target.value)}
-                  className="input-box" placeholder='Enter Country'/>
-          </div>
+    return <>
+        <CheckoutSteps step1 step2 />
+        <div className='max-w-lg mx-auto py-3'>
+            <h1 className='text-3xl mx-1 font-semibold text-gray-600 py-2'>Shipping Address</h1>
+            <form onSubmit={handleSubmit}
+                className='bg-slate-100 p-8 my-2 rounded-md'
+            >
+                <div className="mb-5">
+                    <label className="block mb-2 text-sm font-medium text-gray-500">Address</label>
+                    <input
+                        type="text"
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                        className="input-box" placeholder="Enter Address" />
+                </div>
+                <div className="mb-5">
+                    <label className="block mb-2 text-sm font-medium text-gray-500">City</label>
+                    <input
+                        type="text"
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                        className="input-box" placeholder="Enter City" />
+                </div>
+                <div className="mb-5">
+                    <label className="block mb-2 text-sm font-medium text-gray-500">Pin Code</label>
+                    <input
+                        type="text"
+                        value={pinCode}
+                        onChange={(e) => setPinCode(e.target.value)}
+                        className="input-box" placeholder="Enter Pincode (Postalcode)" />
+                </div>
+                <div className="mb-5">
+                    <label className="block mb-2 text-sm font-medium text-gray-500">Country</label>
+                    <input
+                        type="text"
+                        value={country}
+                        onChange={(e) => setCountry(e.target.value)}
+                        className="input-box" placeholder='Enter Country' />
+                </div>
       
-          <button type="submit" className="btn" >Continue</button>
-      </form>
-  </div>
-</>
+                <button type="submit" className="btn" >Continue</button>
+            </form>
+        </div>
+    </>;
 }
 
 export default ShippingScreen
