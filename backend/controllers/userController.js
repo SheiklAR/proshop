52e3a15ef95237cf370ca46b8e0046c7726aae2e
Sheik Abdul Rahman H <sheikabdulrahman2000@gmail.com>
2024-11-02 17:11:18 +0530

    Removed import name from ejs

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,3 @@
-import { name } from "ejs";
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
 import { generateToken } from "../utils/generateToken.js";
